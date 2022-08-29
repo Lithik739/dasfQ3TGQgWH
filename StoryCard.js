1958bca316dc69a433753b49c1a4c34faf5e6074
Lithik739 <82034445+Lithik739@users.noreply.github.com>
2022-08-29 18:52:49 -0400

    Add files via upload

---
+++ StoryCard.js
@@ -0,0 +1,202 @@
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Platform,
+  StatusBar,
+  Image,
+  Dimensions,
+  TouchableOpacity
+} from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { RFValue } from "react-native-responsive-fontsize";
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+
+let customFonts = {
+  "Bubblegum-Sans": require("../assets/fonts/BubblegumSans-Regular.ttf")
+};
+
+export default class StoryCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fontsLoaded: false,
+      story_id:this.props.story.key,
+      story_data:this.props.story.value,
+      is_liked:false,
+      likes:this.props.story.value.likes
+    };
+  }
+
+  async _loadFontsAsync() {
+    await Font.loadAsync(customFonts);
+    this.setState({ fontsLoaded: true });
+  }
+
+  componentDidMount() {
+    this._loadFontsAsync();
+  }
+
+likeAction=()=>{
+  if()
+
+
+
+
+  
+}
+  
+  fetchUser = () => {
+    let theme;
+    firebase
+      .database()
+      .ref("/users/" + firebase.auth().currentUser.uid)
+      .on("value", snapshot => {
+        theme = snapshot.val().current_theme;
+        this.setState({ light_theme: theme === "light" });
+      });
+  };
+  render() {
+    let story=this.story.story_data
+    if (!this.state.fontsLoaded) {
+      return <AppLoading />;
+    } else {
+let images= {
+  image_1: require("../assets/story_image_1.png"),
+  image_2: require("../assets/story_image_2.png"),
+  image_3: require("../assets/story_image_3.png"),
+  image_4: require("../assets/story_image_4.png"),
+  image_5: require("../assets/story_image_5.png")
+};
+      
+      return (
+        <TouchableOpacity
+          style={styles.container}
+          onPress={() =>
+            this.props.navigation.navigate("StoryScreen", {
+              story: this.props.story
+            })
+          }
+        >
+          <View style={styles.cardContainer}>
+            <Image
+              source={require("../assets/story_image_1.png")}
+              style={styles.storyImage}
+            ></Image>
+
+            <View style={styles.titleContainer}>
+              <Text style={styles.storyTitleText}>
+                {this.props.story.title}
+              </Text>
+              <Text style={styles.storyAuthorText}>
+                {this.props.story.author}
+              </Text>
+              <Text style={styles.descriptionText}>
+                {this.props.story.description}
+              </Text>
+            </View>
+            <View style={styles.actionContainer}>
+<TouchableOpacity onPress={()=>this.likeAction()}
+style={
+  this.state.is_liked
+    ? styles.likeButtonLiked
+    : styles.likeButtonDisliked
+}
+
+>
+              <View style={styles.likeButton}>
+                <Ionicons name={"heart"} size={RFValue(30)} color={"white"} />
+                <Text style={
+                    this.state.light_theme
+                      ? styles.likeTextLight
+                      : styles.likeText}>
+                        
+                       {this.state.likes}
+                        
+                        </Text>
+              </View>
+              </TouchableOpacity>
+              
+            </View>
+          </View>
+        </TouchableOpacity>
+      );
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  cardContainer: {
+    margin: RFValue(13),
+    backgroundColor: "#2f345d",
+    borderRadius: RFValue(20)
+  },
+  storyImage: {
+    resizeMode: "contain",
+    width: "95%",
+    alignSelf: "center",
+    height: RFValue(250)
+  },
+  titleContainer: {
+    paddingLeft: RFValue(20),
+    justifyContent: "center"
+  },
+  storyTitleText: {
+    fontSize: RFValue(25),
+    fontFamily: "Bubblegum-Sans",
+    color: "white"
+  },
+  storyAuthorText: {
+    fontSize: RFValue(18),
+    fontFamily: "Bubblegum-Sans",
+    color: "white"
+  },
+  descriptionText: {
+    fontFamily: "Bubblegum-Sans",
+    fontSize: 13,
+    color: "white",
+    paddingTop: RFValue(10)
+  },
+  actionContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    padding: RFValue(10)
+  },
+  likeButtonLiked: {
+    width: RFValue(160),
+    height: RFValue(40),
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    backgroundColor: "#eb3948",
+    borderRadius: RFValue(30)
+  },
+  likeButtonDisliked: {
+    width: RFValue(160),
+    height: RFValue(40),
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    borderColor: "#eb3948",
+    borderWidth: 2,
+    borderRadius: RFValue(30)
+  },
+  likeText: {
+    color: "white",
+    fontFamily: "Bubblegum-Sans",
+    fontSize: 25,
+    marginLeft: 25,
+    marginTop: 6
+  },
+  likeTextLight: {
+    fontFamily: "Bubblegum-Sans",
+    fontSize: 25,
+    marginLeft: 25,
+    marginTop: 6
+  }
+});
